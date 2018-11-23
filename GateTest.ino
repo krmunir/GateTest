cb9479d1e5190c95df8288c50fd090756a8e2f86
Kash Admin <krmunir@gmail.com>
2018-11-23 12:53:24 +1030

    first commit

---
+++ GateTest.ino
@@ -0,0 +1,42 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       GateTest.ino
+    Created:	Fri 23 Nov 2018 12:43:17 PM
+    Author:     COHDA\kmunir
+*/
+
+/*******************************************************
+
+This code runs on a separate NodeMCU (or similar) and provides :
+- serial output simulating signal from SlidingGateSensor whilst opening and closing
+- led outputs indicating opening / closing
+
+*******************************************************/
+
+
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
